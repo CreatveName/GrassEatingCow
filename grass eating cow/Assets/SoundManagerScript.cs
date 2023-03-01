b53c8cb1161fc00855169fba460bf62a99ad93e6
RonaldEncanto <117491646+RonaldEncanto@users.noreply.github.com>
2023-02-28 17:26:18 -0800

    Swapped Out Sprites
    Commented code for Sound when I get the chance to find it.

--- grass eating cow/Assets/SoundManagerScript.cs
+++ grass eating cow/Assets/SoundManagerScript.cs
@@ -11,7 +11,18 @@ public class SoundManagerScript : MonoBehaviour
     {
         goatSound = Resources.Load<AudioClip>("goat");
 
-            audioSrc = GetComponent<AudioSource> ();
+        //dinoSound = Resources.Load<AudioClip>("dinosaur");
+
+        //cowSound = Resources.Load<AudioClip>("cow");
+
+        //mooshroomSound = Resources.Load<AudioClip>("mooshroom");
+
+        //almondSound = Resources.Load<AudioClip>("nut");
+
+
+        audioSrc = GetComponent<AudioSource> ();
+
+
     }
 
 
@@ -24,6 +35,23 @@ public class SoundManagerScript : MonoBehaviour
             case "goat":
                 audioSrc.PlayOneShot (goatSound);
                 break;
+
+            //case "dinosaur"
+                //audioSrc.PlayOneShot (dinoSound);
+                //break;
+
+            //case "cow"
+                //audioSrc.PlayOneShot(cowSound);
+                //break;
+
+             //case "mooshroom"
+                //audioSrc.PlayOneShot(mooshroomSound);
+                //break;
+
+             //case "nut"
+                //audioSrc.PlayOneShot(almondSound);
+                //break;
+
         }
     }
 }
