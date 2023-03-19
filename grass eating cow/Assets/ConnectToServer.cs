019304be906e328745adb1b79b243bda13929d9f
runzackrun <32371770+runzackrun@users.noreply.github.com>
2023-03-19 01:13:19 -0700

    photon

--- grass eating cow/Assets/ConnectToServer.cs
+++ grass eating cow/Assets/ConnectToServer.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    private void Start()
-    {
 
-        PhotonNetwork.ConnectUsingSettings();
+    public InputField usernameInput;
+    public Text buttonText;
+
+    public void OnClickConnect()
+    {
+        if (usernameInput.text.Length >= 1)
+        {
+            PhotonNetwork.NickName = usernameInput.text;
+
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
+
 
     public override void OnConnectedToMaster()
     {
-        PhotonNetwork.JoinLobby();
-    }
 
-    public override void OnJoinedLobby()
-    {
         SceneManager.LoadScene("Lobby");
     }
 }
+
+
