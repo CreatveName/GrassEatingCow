423d6bd42b2b2610280970f7e4d156b1e9e1f565
CreatveName <trentku10@gmail.com>
2023-03-19 22:38:25 -0700

    audio stuff

--- grass eating cow/Assets/Scripts/Player/PlayerController.cs
+++ grass eating cow/Assets/Scripts/Player/PlayerController.cs
@@ -7,45 +7,54 @@ public class PlayerController : MonoBehaviour
 {
     public int playerNumber = 1;
     public CharStats charStats;
+    [SerializeField]
     private Animator animator;
     public float moveSpeed;
     private Vector2 movement;
+    [SerializeField]
     private Rigidbody2D player;
     private string hAxis;
     private string vAxis;
     PhotonView view;
+    [SerializeField]
+    private AudioSource ad;
+    [SerializeField]
+    private float soundLimit;
+    private float soundTimer;
+
 
 
     void Start()
     {
-        player = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
         hAxis = "Horizontal" + playerNumber;
         vAxis = "Vertical" + playerNumber;
         view = GetComponent<PhotonView>();
+        soundTimer = soundLimit;
     }
     private void Awake()
     {
         moveSpeed = charStats.moveSpeed;
+        ad.clip = charStats.charSound;
     }
     // Tracks the players X and Y values and updates it constantly.
     void Update()
     {
         movement.x = Input.GetAxisRaw(hAxis);
         movement.y = Input.GetAxisRaw(vAxis);
-
-        /*
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
-        */
     }
     // With the new location of X and Y it allows the character to move based off of it's base movespeed.   
     private void FixedUpdate()
-   
-
     {
         player.AddForce(new Vector2(movement.x, movement.y) * moveSpeed, ForceMode2D.Force);
+
+        soundTimer -= Time.fixedDeltaTime;
+
+        if(soundTimer <= 0)
+        {
+            ad.Play();
+            float soundCD = Random.Range(1f, soundLimit);
+            soundTimer = soundCD;
+        }
     }
 
 
